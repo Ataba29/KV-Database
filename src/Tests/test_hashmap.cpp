93cc4b04d52f4f9060d987ddf56aa9e453e0aa59
Ataba <ahmadata178998@gmail.com>
2026-06-28 23:57:25 +0300

    added thread include that was missing

--- src/Tests/test_hashmap.cpp
+++ src/Tests/test_hashmap.cpp
@@ -1,19 +1,21 @@
 #include <gtest/gtest.h>
+#include <thread>
 #include "../RAM/HashMap.h"
 
-
-TEST(HashMapTests, HandlesInsertCorrectly) {
-    HashMap map{ 2 };
+TEST(HashMapTests, HandlesInsertCorrectly)
+{
+    HashMap map{2};
     const std::string key = "key";
     const std::string value = "value";
 
     ASSERT_NO_THROW(map.insert(key, value));
-    
+
     EXPECT_EQ(map.get(key), value);
 }
 
-TEST(HashMapTests, HandlesGetWithDuplicateKeyInsertions) {
-    HashMap map{ 3 };
+TEST(HashMapTests, HandlesGetWithDuplicateKeyInsertions)
+{
+    HashMap map{3};
 
     const std::string key = "key";
 
@@ -27,70 +29,78 @@ TEST(HashMapTests, HandlesGetWithDuplicateKeyInsertions) {
     EXPECT_EQ(map.get(key), value2);
 }
 
-
-TEST(HashMapTests, HandledRemoveCorrectly) {
-    HashMap map{ 2 };
+TEST(HashMapTests, HandledRemoveCorrectly)
+{
+    HashMap map{2};
     const std::string key = "key";
     const std::string value = "value";
 
     ASSERT_NO_THROW(map.insert(key, value));
 
     ASSERT_NO_THROW(map.remove(key));
-    
+
     EXPECT_EQ(map.get(key), "");
 }
 
-
-TEST(HashMapTests, HandledRemoveWhenKeyIsntInHashmap) {
-    HashMap map{ 2 };
+TEST(HashMapTests, HandledRemoveWhenKeyIsntInHashmap)
+{
+    HashMap map{2};
 
     const std::string key = "key";
     const std::string value = "value";
     ASSERT_NO_THROW(map.insert(key, value));
 
     ASSERT_NO_THROW(map.remove("Random Key"));
-    
+
     EXPECT_EQ(map.get(key), value);
 }
 
-TEST(HashMapConcurrencyTests, HandlesConcurrentInsertsSafely) {
-    HashMap map{ 5 }; // The map is small to make sure that there is many collitions
+TEST(HashMapConcurrencyTests, HandlesConcurrentInsertsSafely)
+{
+    HashMap map{5}; // The map is small to make sure that there is many collitions
     const int num_threads = 5;
     const int inserts_per_thread = 100;
-    
-    std::vector<std::thread> threads;
-	std::atomic<bool> gate{ false };
 
-    auto worker = [&map, &gate](int thread_id, int inserts) {
-		while (!gate.load()) {
+    std::vector<std::thread> threads;
+    std::atomic<bool> gate{false};
+
+    auto worker = [&map, &gate](int thread_id, int inserts)
+    {
+        while (!gate.load())
+        {
             // Created a Spinner Lock (Were threads are hot and ready to fire!)
-		}
-		for (int i{}; i < inserts; ++i) {
-			std::string unique_key = "thread_" + std::to_string(thread_id) + "_key_" + std::to_string(i);
-			std::string value = "value_" + std::to_string(i);
+        }
+        for (int i{}; i < inserts; ++i)
+        {
+            std::string unique_key = "thread_" + std::to_string(thread_id) + "_key_" + std::to_string(i);
+            std::string value = "value_" + std::to_string(i);
 
             map.insert(unique_key, value);
         }
     };
 
     // At this point we spawn the threads and let them spin
-    for (int i{}; i < num_threads; ++i) {
+    for (int i{}; i < num_threads; ++i)
+    {
         threads.emplace_back(worker, i, inserts_per_thread);
     }
 
-	// Open the floodgates!
+    // Open the floodgates!
     gate.store(true);
 
-    for (auto& t : threads) {
+    for (auto &t : threads)
+    {
         t.join();
     }
 
-    for (int t{}; t < num_threads; ++t) {
-        for (int i{}; i < inserts_per_thread; ++i) {
+    for (int t{}; t < num_threads; ++t)
+    {
+        for (int i{}; i < inserts_per_thread; ++i)
+        {
             std::string expected_key = "thread_" + std::to_string(t) + "_key_" + std::to_string(i);
             std::string expected_value = "value_" + std::to_string(i);
-            
-            EXPECT_EQ(map.get(expected_key), expected_value) 
+
+            EXPECT_EQ(map.get(expected_key), expected_value)
                 << "Missing data from thread " << t << " at iteration " << i;
         }
     }
