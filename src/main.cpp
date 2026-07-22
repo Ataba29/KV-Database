b8f29f6c716c06d8ba3463099861cda9a5efb288
Ataba <ahmadata178998@gmail.com>
2026-07-22 09:06:43 +0300

    Added watching kill signal and shutdown gracefully

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
+#include <atomic>
+#include <csignal>
 #include <iostream>
 #include <string>
 #include <thread>
+#include <chrono>
 
 #include "RAM/HashMap.h"
 #include "Server/Server.h"
+
+namespace
+{
+    std::atomic<bool> shutdownRequested{false};
+    void handleSignal(int)
+    {
+        shutdownRequested.store(true, std::memory_order_relaxed);
+    }
+}
 
 int main()
 {
@@ -13,29 +25,40 @@ int main()
 
     server.start();
 
+    std::signal(SIGTERM, handleSignal);
+    std::signal(SIGINT, handleSignal);
+
     // Run accept loop in background thread
     std::thread serverThread(&Server::acceptClients, &server);
 
     std::cout << "[MAIN] Server running. Type 'stop' to shut it down.\n";
 
-    std::string cmd;
-
-    while (true)
-    {
-        std::cin >> cmd;
-
-        if (cmd == "stop")
+    std::thread stdinThread([]{
+        std::string cmd;
+        while(!shutdownRequested.load(std::memory_order_relaxed))
         {
-            std::cout << "[MAIN] Stopping server...\n";
-
-            server.stop();
-
-            break;
+            // The application was ran from docker
+            if(!(std::cin >> cmd))
+                break;
+            
+            // This application is running on terminal use old logic to shut down
+            if(cmd == "stop"){
+                shutdownRequested.store(true, std::memory_order_relaxed);
+                break;
+            }
         }
+    });
+
+    while(!shutdownRequested.load(std::memory_order_relaxed)){
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    
+    std::cout << "[MAIN] Stopping server...\n";
+    server.stop();
 
     // Wait for server thread to finish
     serverThread.join();
+    stdinThread.detach();
 
     std::cout << "[MAIN] Server exited cleanly\n";
 
